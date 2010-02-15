7f45a689b2a1dadb1d397155d7a940ec785f1607
Travis Swicegood <development@domain51.com>
2010-02-15 14:47:32 -0600

    allow variable names inside the hit logger

--- d51/django/apps/logger/templatetags/hit_logger.py
+++ d51/django/apps/logger/templatetags/hit_logger.py
@@ -5,15 +5,29 @@ from d51.django.apps.logger.views import hit
 register = template.Library()
 
 class HitLoggerNode(template.Node):
-    def __init__(self, url):
+    def __init__(self, url, context_name=None):
         self.url = url
+        self.context_name = context_name
 
     def render(self, context):
-        return reverse(hit, kwargs={"url": self.url})
+        if self.url.find(":") == -1:
+            # assume we have a variable
+            self.url = template.Variable(self.url).resolve(context)
+        url = reverse(hit, kwargs={"url": self.url})
+        if self.context_name:
+            context[self.context_name] = url
+        else:
+            return url
 
 @register.tag('log_hit')
 def do_log_hit(parser, token):
     # TODO: add support for block parameters
     # TODO: add support for variable URLs
-    [_tag_name, url] = token.split_contents()
-    return HitLoggerNode(url)
+    tokens = token.split_contents()
+    url = None
+    context_name = None
+    if len(tokens) == 2:
+        [_tag_name, url] = tokens
+    else:
+        [_tag_name, url, _as, context_name] = tokens
+    return HitLoggerNode(url, context_name)
