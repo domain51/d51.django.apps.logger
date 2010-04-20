a4afa26610fcf8bde2c3c5c54f326c541796cc59
Travis Swicegood <development@domain51.com>
2010-04-20 11:08:36 -0500

    make sure to pass the GET params along

--- d51/django/apps/logger/views.py
+++ d51/django/apps/logger/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponseRedirect
+import urllib
 
 from .models import Hit
 from .utils import HIT_RELATED_TO
 
 def hit(request, url):
+    if request.GET.items():
+        url = "%s?%s" % (url, urllib.urlencode(request.GET.items()))
     hit = Hit(url=url)
     if HIT_RELATED_TO in request.session:
         related_to = request.session[HIT_RELATED_TO]
